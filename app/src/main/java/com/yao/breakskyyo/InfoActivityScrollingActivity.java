b091374a66d3620d9e94b954beed989d76868494
ly <ly@jianzhimao.com>
2015-09-22 22:40:12 +0800

    展示信息

--- app/src/main/java/com/yao/breakskyyo/InfoActivityScrollingActivity.java
+++ app/src/main/java/com/yao/breakskyyo/InfoActivityScrollingActivity.java
@@ -7,10 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.alibaba.fastjson.JSON;
+import com.yao.breakskyyo.adapter.LinearLayoutAddViewAdapter;
+import com.yao.breakskyyo.dummy.DownloadInfoItem;
 import com.yao.breakskyyo.dummy.DummyContent;
 import com.yao.breakskyyo.dummy.InfoVideos;
 import com.yao.breakskyyo.tools.RegularId97;
@@ -23,18 +27,18 @@ import org.kymjs.kjframe.ui.ViewInject;
 import org.kymjs.kjframe.utils.KJLoger;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.MatchResult;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class InfoActivityScrollingActivity extends AppCompatActivity {
     DummyContent.DummyItem mDummyItem;
     InfoVideos mInfoVideos;
     ImageView showImg;
     TextView tag;
+    LinearLayoutAddViewAdapter  adapter;
+    LinearLayout resource_download_list;
+    TextView plot_introduction;
+   // Button play_bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +60,10 @@ public class InfoActivityScrollingActivity extends AppCompatActivity {
     private void init(){
         showImg=(ImageView)findViewById(R.id.showImg);
         tag=(TextView)findViewById(R.id.tag);
+        resource_download_list=(LinearLayout)findViewById(R.id.resource_download_list);
+        plot_introduction=(TextView)findViewById(R.id.plot_introduction);
+        //play_bt=(Button)findViewById(R.id.play_bt);
+        adapter = new LinearLayoutAddViewAdapter(InfoActivityScrollingActivity.this, resource_download_list.getId());
         String jsonFindItemInfo=getIntent().getStringExtra("jsonFindItemInfo");
         if(TextUtils.isEmpty(jsonFindItemInfo)){
             finish();
@@ -73,7 +81,7 @@ public class InfoActivityScrollingActivity extends AppCompatActivity {
 
     public void httpGetItemInfo() {
         KJHttp kjh = new KJHttp();
-        kjh.get((String)mDummyItem.getUrl(), new HttpCallBack() {
+        kjh.get((String) mDummyItem.getUrl(), new HttpCallBack() {
             @Override
             public void onPreStart() {
                 super.onPreStart();
@@ -84,10 +92,10 @@ public class InfoActivityScrollingActivity extends AppCompatActivity {
             public void onSuccess(String t) {
                 super.onSuccess(t);
                 ViewInject.longToast("请求成功");
-                KJLoger.debug("yoyo 结果:"+(String)mDummyItem.getUrl()+"--"+ t);
+                KJLoger.debug("yoyo 结果:" + (String) mDummyItem.getUrl() + "--" + t);
 
-                mInfoVideos= RegularId97.getInfoVideos(t);
-
+                mInfoVideos = RegularId97.getInfoVideos(t);
+                initInfoVideos();
             }
 
             @Override
@@ -105,6 +113,32 @@ public class InfoActivityScrollingActivity extends AppCompatActivity {
 
 
         });
+
+    }
+    public void initInfoVideos(){
+        plot_introduction.setText(mInfoVideos.getMovie_jvqing());
+        List<DownloadInfoItem> downloadInfoItemList=new ArrayList<>();
+        if(!TextUtils.isEmpty(mInfoVideos.getBaiduPanUrl())){
+            downloadInfoItemList.add(new DownloadInfoItem(mInfoVideos.getBaiduPanName(),mInfoVideos.getBaiduPanUrl(),mInfoVideos.getBaiduPanUrlMima()));
+        }
+        if( mInfoVideos.getRegularChili()!=null){
+            for ( Map<String,String> map:
+                    mInfoVideos.getRegularChili()) {
+                downloadInfoItemList.add(new DownloadInfoItem(map.get("url"),map.get("name"),null));
+            }
+        }
+        adapter.clearmdata();
+        adapter.addmdata(downloadInfoItemList);
+        adapter.addCommentView();
+
+    }
+    public void onClickDo(View view){
+        switch (view.getId()){
+            case R.id.play_bt:
+
+                break;
+
+        }
 
     }
 
