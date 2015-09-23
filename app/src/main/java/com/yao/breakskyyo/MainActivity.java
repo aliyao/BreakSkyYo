7098144d47b4378e77dcad854cdf9a919b989df0
ly <ly@jianzhimao.com>
2015-09-23 20:57:02 +0800

    fab.setOnClickListener

--- app/src/main/java/com/yao/breakskyyo/MainActivity.java
+++ app/src/main/java/com/yao/breakskyyo/MainActivity.java
@@ -57,8 +57,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.openDrawer(GravityCompat.START);
             }
         });
 
