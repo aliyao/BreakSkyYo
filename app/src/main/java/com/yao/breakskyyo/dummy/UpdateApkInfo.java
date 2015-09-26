a31e6db4a1e655ef3792de59a869e97944d23324
ly <ly@jianzhimao.com>
2015-09-26 13:05:43 +0800

    更新

--- app/src/main/java/com/yao/breakskyyo/dummy/UpdateApkInfo.java
+++ app/src/main/java/com/yao/breakskyyo/dummy/UpdateApkInfo.java
@@ -1,11 +1,18 @@
 package com.yao.breakskyyo.dummy;
 
 /**
- * Created by nideyoyo on 2015/9/24.
+ * 项目名称：BreakSkyYo
+ * 类描述：
+ * 创建人：yoyo
+ * 创建时间：2015/9/25 16:41
+ * 修改人：yoyo
+ * 修改时间：2015/9/25 16:41
+ * 修改备注：
  */
 public class UpdateApkInfo {
     int versionCode;
     String url;
+    String description;
 
     public int getVersionCode() {
         return versionCode;
@@ -21,5 +28,13 @@ public class UpdateApkInfo {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
