fab46df1d8bc2b3f5c7c6c0e297e3ceb727f85be
nideyoyo <90508312@qq.com>
2015-10-12 12:51:05 +0800

    优化

--- app/src/main/java/com/yao/breakskyyo/SearchActivity.java
+++ app/src/main/java/com/yao/breakskyyo/SearchActivity.java
@@ -44,7 +44,7 @@ public class SearchActivity extends AppCompatActivity {
     SwipeRefreshLayout refreshView;
     private ArrayAdapter<SearchItem> mAdapter;
     EditText et_word;
-   Button bt_search;
+    Button bt_search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,14 +61,15 @@ public class SearchActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
-        mListView=(ListView)findViewById(R.id.lv_search_list);
+        mListView = (ListView) findViewById(R.id.lv_search_list);
 
-        refreshView=(SwipeRefreshLayout)findViewById(R.id.refreshView);
-        mAdapter = new ArrayAdapter<SearchItem>(SearchActivity.this,R.layout.search_item){
+        refreshView = (SwipeRefreshLayout) findViewById(R.id.refreshView);
+        mAdapter = new ArrayAdapter<SearchItem>(SearchActivity.this, R.layout.search_item) {
             @Override
             public SearchItem getItem(int position) {
                 return super.getItem(position);
             }
+
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
                 View view;
@@ -77,25 +78,25 @@ public class SearchActivity extends AppCompatActivity {
                 } else {
                     view = convertView;
                 }
-                SearchItem mSearchItem=getItem( position);
-                TextView tv_hdtag=(TextView)view.findViewById(R.id.tv_hdtag);
-                TextView tv_content=(TextView)view.findViewById(R.id.tv_content);
-                ImageView iv_image=(ImageView)view.findViewById(R.id.iv_image);
+                SearchItem mSearchItem = getItem(position);
+                TextView tv_hdtag = (TextView) view.findViewById(R.id.tv_hdtag);
+                TextView tv_content = (TextView) view.findViewById(R.id.tv_content);
+                ImageView iv_image = (ImageView) view.findViewById(R.id.iv_image);
                 if (YOBitmap.getmKJBitmap().getCache(StringDo.removeNull(mSearchItem.getImgUrl())).length <= 0) {
                     iv_image.setImageBitmap(null);
                 }
-                YOBitmap.getmKJBitmap().display(iv_image,mSearchItem.getImgUrl());
+                YOBitmap.getmKJBitmap().display(iv_image, mSearchItem.getImgUrl());
                 tv_hdtag.setText(mSearchItem.getHdtag());
                 String strHtml = mSearchItem.getContent();
-                if(!TextUtils.isEmpty(strHtml)){
+                if (!TextUtils.isEmpty(strHtml)) {
                     tv_content.setText(Html.fromHtml(strHtml));
                 }
                 return view;
             }
         };
         View headerView = View.inflate(this, R.layout.search_item_head, null);
-        et_word=(EditText)headerView.findViewById(R.id.et_word);
-        bt_search=(Button)headerView.findViewById(R.id.bt_search);
+        et_word = (EditText) headerView.findViewById(R.id.et_word);
+        bt_search = (Button) headerView.findViewById(R.id.bt_search);
         bt_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -118,30 +119,29 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
     }
+
     // 隐藏键盘
     private void hintKb() {
 
 
-            InputMethodManager imm =  (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
-            if(imm != null) {
-                imm.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(),
-                        0);
-            }
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(),
+                    0);
+        }
 
     }
 
     public void httpGetSearchInfo() {
-        String word=et_word.getText()+"";
-        if(TextUtils.isEmpty(word)){
+        String word = et_word.getText() + "";
+        if (TextUtils.isEmpty(word)) {
             Snackbar.make(et_word, "请输入关键字", Snackbar.LENGTH_LONG).show();
-            if (!refreshView.isRefreshing()) {
-                refreshView.setRefreshing(false);
-            }
+            refreshView.setRefreshing(false);
             return;
         }
         try {
-            word= URLEncoder.encode(word,"utf-8");
-        }catch (Exception e){
+            word = URLEncoder.encode(word, "utf-8");
+        } catch (Exception e) {
             e.printStackTrace();
         }
         KJHttp kjh = new KJHttp();
@@ -163,11 +163,11 @@ public class SearchActivity extends AppCompatActivity {
                 super.onSuccess(t);
                 ViewInject.longToast("请求成功");
                 KJLoger.debug("log:" + t.toString());
-                List<SearchItem> searchItemList= RegularId97.getSearchItem(t);
+                List<SearchItem> searchItemList = RegularId97.getSearchItem(t);
                 mAdapter.clear();
-                if(searchItemList!=null&&searchItemList.size()>0){
+                if (searchItemList != null && searchItemList.size() > 0) {
                     mAdapter.addAll(searchItemList);
-                }else {
+                } else {
                     Snackbar.make(et_word, "没有找到你想看的电影", Snackbar.LENGTH_LONG).show();
                 }
                 mAdapter.notifyDataSetChanged();
@@ -197,8 +197,9 @@ public class SearchActivity extends AppCompatActivity {
         });
 
     }
-    public void onclick(View view){
-        switch (view.getId()){
+
+    public void onclick(View view) {
+        switch (view.getId()) {
             case R.id.bt_search:
                 httpGetSearchInfo();
                 break;
