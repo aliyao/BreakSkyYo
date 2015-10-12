414d8c9881b59595bf26e2d5a1b5009b99404900
ly <ly@jianzhimao.com>
2015-10-13 03:17:37 +0800

    help页面

--- app/src/main/java/com/yao/breakskyyo/HelpActivity.java
+++ app/src/main/java/com/yao/breakskyyo/HelpActivity.java
@@ -1,5 +1,6 @@
 package com.yao.breakskyyo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.yao.breakskyyo.net.HttpUrl;
+import com.yao.breakskyyo.webview.WebViewActivity;
 
 public class HelpActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -88,7 +92,7 @@ public class HelpActivity extends AppCompatActivity
         } else if (id == R.id.nav_xunlei) {
 
         } else if (id == R.id.nav_computer) {
-
+            startActivity(new Intent(this, WebViewActivity.class).putExtra("url", HttpUrl.HellpUrl));
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
